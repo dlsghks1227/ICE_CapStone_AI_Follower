9b1472b499cf8c06c62487dd7dfc44f26ac0ac44
Banana <lh1227@yahoo.co.kr>
2020-11-04 14:04:11 +0900

    [Server] 서버 프로그램 레이아웃 구성
    #detail
    1. WPF를 이용하여 레이아웃 구성
    2. gitignore 수정

---
+++ BotLine/BotLine/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using BotLine.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BotLine
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 스크롤 뷰어 항상 밑으로 설정
+            SV_MessageBoxScroll.ScrollToBottom();
+
+            // 서버 아이피 주소
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            TB_IP.Text = host.AddressList[1].ToString();
+
+            MessageControl.Print();
+        }
+
+        private void BTN_Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
